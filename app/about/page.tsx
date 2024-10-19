9fa0e980861709bec1d878541eb15e15e8fa93fd
Sebastiaan Colmen <skolmschate@hotmail.nl>
2024-10-19 14:55:50 +0200

    added link to contact page in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,13 @@
-// components/About.tsx
 import React from 'react';
 
-const About: React.FC = () => {
+import Link from 'next/link';
+
+export default function About() {
   const birthDate = new Date(2000, 6, 3); // Month is 0-indexed
   const age = new Date().getFullYear() - birthDate.getFullYear();
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-neutral-800 text-neutral-200 rounded-lg shadow-md">
+    <div className="flex flex-col gap-3 max-w-2xl mx-auto p-6 text-neutral-200">
       <h1 className="text-3xl font-bold mb-4">About Me</h1>
       <p className="mb-2">
         Hello, I'm Sebastiaan Kolmschate, a {age} years old student at Windesheim University of Applied Sciences.
@@ -14,8 +15,9 @@ const About: React.FC = () => {
       <p>
         I have experience in full-stack software development, with a current specialization in machine learning and its applications in software solutions.
       </p>
+      <p>
+        Use the <Link href="/contact" className="underline">contact form</Link> to get in touch with me.
+      </p>
     </div>
   );
-};
-
-export default About;
+}
